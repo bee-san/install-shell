ba8ba5fe029b607d772525d1accf22d3ca7eebed
Brandon <brandonskerritt51@gmail.com>
2015-11-29 21:28:26 +0000

    first commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+clear
+sudo apt-get install guake -y
+sudo apt-get install macchanger
+sudo apt-get install git -y
+
+clear
+echo "Install.sh has completed. Please exit."
