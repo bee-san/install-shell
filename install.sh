8da5bff35ecdd2d96c1e05a5125dedaa08b3f8f6
Brandon <brandonskerritt51@gmail.com>
2015-11-29 21:41:48 +0000

    1

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ clear
 sudo apt-get install guake -y
 sudo apt-get install macchanger
 sudo apt-get install git -y
-
+sudo apt-get install gimp -y
+sudo apt-add-repository ppa:numix/ppa -y
+sudo apt-get update
+sudo apt-get install numix-icon-theme-circle -y
 clear
 echo "Install.sh has completed. Please exit."
